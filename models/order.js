0ec4b832a9bcfeb2a5b8919cc626c1d04485d237
ASDT15 <ahmedalsofi1@gmail.com>
2025-07-31 00:02:05 +0300

    fix: connect to Render PostgreSQL with SSL

--- models/order.js
+++ models/order.js
@@ -119,6 +119,18 @@ const Order = {
     }
   }
 };
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // مهم لـ Render
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
 
 
 module.exports = Order;
--- models/product.js
+++ models/product.js
@@ -145,5 +145,16 @@ const Product = {
   }
 };
 
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // مهم لـ Render
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
 module.exports = Product;
